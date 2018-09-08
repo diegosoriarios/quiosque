5f965e0ff4339b1e7ab72acd03a87bd251acda3c
Diego Soria Rios <diegosoriarios@gmail.com>
2018-09-08 00:07:11 -0300

    Adicionado a galeria

---
+++ js/galeria.js
@@ -0,0 +1,49 @@
+$( document ).ready(function() {
+    let fotos = [];
+    const TAM = 28;
+    for(let i = 0; i <= TAM; i++){
+    	let foto = './fotos/' + (i+1) + '.jpg';
+    	$("#hexGrid").append("<li class='hex'><div class='hexIn'><a class='hexLink' href='#' onclick='zoom("+ (i+1) + ");'><img id='imagem' src='" + foto + "' /></a></div></li>"
+    	);
+    }
+    $("#proxima").click(function(){
+    	let x = $("#atual").attr('src');
+    	let atual = x.split('/');
+    	atual = (atual[2].split('.')[0]);
+    	if(atual < TAM){
+			$(".popimg").html("<img id='atual' src=./fotos/" + (parseInt(atual)+1) + ".jpg/>");
+    	}else{
+    		$(".popimg").html("<img id='atual' src=./fotos/" + 1 + ".jpg/>");
+		}
+    });
+
+    $("#anterior").click(function(){
+    	let x = $("#atual").attr('src');
+    	let atual = x.split('/');
+    	atual = (atual[2].split('.')[0]);
+    	if(atual > 0){
+			$(".popimg").html("<img id='atual' src=./fotos/" + (parseInt(atual)-1) + ".jpg />");
+    	}else{
+    		$(".popimg").html("<img id='atual' src=./fotos/" + TAM + ".jpg/>");
+		}
+    });
+
+    $("#hexGrid").click(function(){
+        $("body").css("overflow", "hidden");
+    });
+
+});
+
+function zoom(img){
+	$(".popimg").html("<img id='atual' src=./fotos/" + img + ".jpg/>");
+	$(".popup").css("width", "100%");	
+}
+
+function fechar(){
+	$(".popup").css("width", "0");
+    $("body").css("overflow", "visible");
+}
+
+function proxima(imagem){
+	$(".popimg").html("<img src=./fotos/" + (img + 1) + ".jpg/>");
+}
--- js/script.js
+++ js/script.js
@@ -1,103 +1,30 @@
+function arrumar(opcao){
+  $(opcao).css("width", "100%");
+}
 function teste(opcao){
       switch (opcao){
-        case 1:
-          document.getElementById("violao").style.width = "100%";
-          break;
-        case 2:
-          document.getElementById("cirandar").style.width = "100%";
-          break;
-        case 3:
-          document.getElementById("danca").style.width = "100%";
-          break;
-        case 4:
-          document.getElementById("dj").style.width = "100%";
-          break;
-        case 5:
-          document.getElementById("musicando").style.width = "100%";
-          break;
-        case 6:
-          document.getElementById("teatro").style.width = "100%";
-          break;
-        case 7:
-          document.getElementById("texto").style.width = "100%";
-          break;
-        case 8:
-          document.getElementById("xadrez").style.width = "100%";
-          break;
-        case 9:
-          document.getElementById("artes").style.width = "100%";
-          break;
-        case 10:
-          document.getElementById("contato").style.width = "100%";
-          break;
-        case 11:
-          document.getElementById("recados").style.width = "100%";
-          break;
-        case 12:
-            document.getElementById("colaboradores").style.width = "100%";
-            break;
-        case 13:
-              document.getElementById("mantenedores").style.width = "100%";
-              break;
+        case 1:arrumar("#violao"); break;
+        case 2:arrumar("#cirandar");break;
+        case 3:arrumar("#danca");break;
+        case 4:arrumar("#dj");break;
+        case 5:arrumar("#musicando");break;
+        case 6:arrumar("#teatro");break;
+        case 7:arrumar("#texto");break;
+        case 8:arrumar("#xadrez");break;
+        case 9:arrumar("#artes");break;
+        case 10:arrumar("#contato");break;
+        case 11:arrumar("#recados");break;
+        case 12:arrumar("#colaboradores");break;
+        case 13:arrumar("#mantenedores");break;
+        case 14:arrumar("#galeria");break;
 
       }
     }
+    $(".overlay").click(function(){
+      let fechar = '#' + $(this).attr('id');
+      $(fechar).css("width", "0");
+    });
 
-  function closeViolao() {
-      document.getElementById("violao").style.width = "0%";
-  }
-
-  function closeCirandar() {
-      document.getElementById("cirandar").style.width = "0%";
-  }
-
-  $('.hexLink').click(function(e) {
-    e.preventDefault();
-  });
-
-  function closeDanca() {
-      document.getElementById("danca").style.width = "0%";
-  }
-
-  function closeDj() {
-      document.getElementById("dj").style.width = "0%";
-  }
-
-  function closeMusicando() {
-      document.getElementById("musicando").style.width = "0%";
-  }
-
-  function closeTeatro() {
-      document.getElementById("teatro").style.width = "0%";
-  }
-
-  function closeTexto() {
-      document.getElementById("texto").style.width = "0%";
-  }
-
-  function closeXadrez() {
-      document.getElementById("xadrez").style.width = "0%";
-  }
-
-  function closeArtes() {
-      document.getElementById("artes").style.width = "0%";
-  }
-
-  function closeContato() {
-      document.getElementById("contato").style.width = "0%";
-  }
-
-  function closeRecados() {
-      document.getElementById("recados").style.width = "0%";
-  }
-
-  function closeColaboradores() {
-      document.getElementById("colaboradores").style.width = "0%";
-  }
-
-  function closeMantenedores() {
-      document.getElementById("mantenedores").style.width = "0%";
-  }
 
 //TITULO
 var space = " ";
@@ -107,11 +34,20 @@ var space = " ";
   function scroll(){
     document.title = msg.substring(pos, msg.length) + space;
     pos++;
-    if (pos > msg.length + 0) pos = -20;
+    if (pos > msg.length + 0) pos = -20;{
       window.setTimeout(scroll, speed);
+    }
   }
-  scroll();
+scroll();
 
-  $('.hexLink').click(function(e) {
-    e.preventDefault();
-  });
+$('.hexLink').click(function(e) {
+  e.preventDefault();
+});
+
+$(".hexLink").click(function (){
+  $("body").css("overflow", "hidden");
+});
+
+$(".closebtn").click(function(){
+  $("body").css("overflow", "visible");
+})
